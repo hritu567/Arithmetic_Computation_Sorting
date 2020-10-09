904603a8751c0b4726bae0b19c8967d7672f1a13
hritu567 <hritumog96@gmail.com>
2020-10-10 02:58:20 +0530

    Added : stored in dictionary for every computation

--- abc.sh
+++ abc.sh
@@ -2,5 +2,9 @@
 read -p "Enter first number : " a
 read -p "Enter second number : " b
 read -p "Enter third number : " c
-sum=$((a%b+c))
-echo $sum
+declare -A results
+results[sum1]=$((a+b*c))
+results[sum2]=$((a*b+c))
+results[sum3]=$((c+a/b))
+results[sum4]=$((a%b+c))
+echo ${results[@]}
