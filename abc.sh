eaf64b56f5aa8a5b5ede75c75b4a4b5cdebaeed2
hritu567 <hritumog96@gmail.com>
2020-10-10 08:43:51 +0530

    Added : dictionary to array

--- abc.sh
+++ abc.sh
@@ -1,10 +1,56 @@
 #!/bin/bash -x
-read -p "Enter first number : " a
-read -p "Enter second number : " b
-read -p "Enter third number : " c
-declare -A results
-results[sum1]=$((a+b*c))
-results[sum2]=$((a*b+c))
-results[sum3]=$((c+a/b))
-results[sum4]=$((a%b+c))
-echo ${results[@]}
+
+
+read -p "Enter The First Number : " a
+
+read -p "Enter The Second Number : " b
+
+read -p "Enter The Third Number : " c
+
+res1=$(($a+($b*$c)))
+
+echo $res1
+
+res2=$(($a*($b+$c)))
+
+echo $res2
+
+res3=$(($c+($a/$b)))
+
+echo $res3
+
+res4=$(($a%($b+$c)))
+
+echo $res4
+
+
+declare -A array
+
+j=1
+
+for i in res1 res2 res3 res4
+
+do
+	
+array[$j]=$i
+	
+((j++)) 
+
+
+done
+
+echo ${array[@]}
+
+echo ${!array[@]}
+
+
+for ((i=0;i<=${#array[@]};i++))
+
+do
+	
+array1[$i]=${array[$i]}	
+
+
+done
+
+echo ${array1[@]}
